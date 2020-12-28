4291d98fe2e3f838db2dca4224e9d23e751de7d9
Lenni Korhonen <lenni.korhonen2@gmail.com>
2020-12-28 19:24:26 +0200

    Minor changes

--- check.py
+++ check.py
@@ -6,3 +6,6 @@ if (urllib.request.urlopen(URL).getcode() == 200):
     print("Site is up, site connection code:", urllib.request.urlopen(URL).getcode())
 else:
     print(urllib.request.urlopen(URL).getcode())
+
+
+print(urllib.request.urlopen(URL))
